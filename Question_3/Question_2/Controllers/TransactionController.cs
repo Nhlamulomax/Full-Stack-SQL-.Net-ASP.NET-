abdd2190268f3d3cac9b9e9f6f267bdd822204f0
Nhlamulo Mabunda <nhlamulomax@gmail.com>
2019-02-16 08:52:41 +0200

    adding my full stack project

---
+++ Question_3/Question_2/Controllers/TransactionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Question_2.Models;
+
+namespace Question_2.Controllers
+{
+    public class TransactionController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            List<Transaction> transactionList = new List<Transaction>();
+            using (DBModel dc = new DBModel())
+            {
+                transactionList = dc.Transactions.OrderBy(a => a.AccountNumber).ToList();
+                HttpResponseMessage response;
+                response = Request.CreateResponse(HttpStatusCode.OK, transactionList);
+                return response;
+            }
+        }
+
+    }
+}
